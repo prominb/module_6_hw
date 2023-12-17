3d8c769aa54d8cc88e398161c954b502a91b32e5
Roman_M <prominb@gmail.com>
2023-12-17 06:58:45 +0200

    scan folders done

---
+++ scan.py
@@ -0,0 +1,77 @@
+import sys
+from pathlib import Path
+
+
+jpeg_files = list()
+png_files = list()
+jpg_files = list()
+svg_files = list()
+
+avi_files = list()
+mp4_files = list()
+mov_files = list()
+mkv_files = list()
+
+doc_files = list()
+docx_files = list()
+txt_files = list()
+pdf_files = list()
+xlsx_files = list()
+pptx_files = list()
+
+mp3_files = list()
+ogg_files = list()
+wav_files = list()
+amr_files = list()
+
+zip_files = list()
+gz_files = list()
+tar_files = list()
+
+unknown = set()
+unknown_files = list()
+
+extensions = set()
+folders = list()
+
+registered_extensions = {
+    'JPEG': jpeg_files, 'PNG': png_files, 'JPG': jpg_files, 'SVG': svg_files,
+    'AVI': avi_files, 'MP4': mp4_files, 'MOV': mov_files, 'MKV': mkv_files,
+    'DOC': doc_files, 'DOCX': docx_files, 'TXT': txt_files, 'PDF': pdf_files, 'XLSX': xlsx_files, 'PPTX': pptx_files,
+    'MP3': mp3_files, 'OGG': ogg_files, 'WAV': wav_files, 'AMR': amr_files,
+    'ZIP': zip_files, 'GZ': gz_files, 'TAR': tar_files
+}
+
+
+def get_extensions(file_name):
+    return Path(file_name).suffix[1:].upper()
+
+
+def scan(folder):
+    for item in folder.iterdir():
+        if item.is_dir():
+            if item.name not in ('Pictures', 'Video', 'Documents', 'Music', 'Archives', 'Unknown'):
+                folders.append(item)
+                scan(item)
+            continue
+
+        extension = get_extensions(file_name=item.name)
+        new_file_name = folder/item.name
+
+        if not extension:
+            unknown_files.append(new_file_name)
+        else:
+            try:
+                container = registered_extensions[extension]
+                extensions.add(extension)
+                container.append(new_file_name)
+            except KeyError:
+                unknown.add(extension)
+                unknown_files.append(new_file_name)
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    print(f"Start in folder name: {path}")
+    folder = Path(path)
+    scan(folder)
